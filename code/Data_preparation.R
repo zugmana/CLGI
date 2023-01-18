ca085ad2506f343e4dba81f3934657ecd703ae7e
Andre  Zugman <andre.zugman@gmail.com>
2023-01-18 15:22:36 -0500

    updates

---
+++ code/Data_preparation.R
@@ -0,0 +1,221 @@
+#This script will get the data for GI and organize for further use.
+#Created by N. Crossley, U.C. Chile, modified by A. Zugman, NIMH/NIH
+# Not all data used in the paper can be shared due to individual countries regulation.
+##"Country-level gender inequality is associated with structural differences in the 
+#brains of women and men"
+#
+#Library to set wd
+library(rstudioapi)
+
+#------------- INPUT NEEDED---------------------
+# Change the directory as needed
+script_path <- dirname( rstudioapi::getActiveDocumentContext()$path )
+setwd(script_path)
+setwd("..")
+rootdir <- getwd()
+#-----------------------------------------------
+
+# List of included studies
+COHT<-read.csv(paste0(rootdir,'/','tables','/','TABLE_all_used_v2.csv'))
+
+# ------------------------------------------------------------------------------
+## The Gender inequality index combined
+# Average of the z-scores (flip signal for World Bank)
+
+# UNDP
+GIt=read.csv(paste0(rootdir,'/','tables','/',"2020_statistical_annex_table5_GI.csv"))
+temp1=as.numeric(GIt$GI2019)
+zundp=(temp1-mean(temp1,na.rm=TRUE))/sd(temp1,na.rm=TRUE)
+rm(temp1)
+
+# World Bank
+GIto2=read.csv(paste0(rootdir,'/','tables','/',"data_WB.csv"))
+t1=which(GIto2$Indicator=="Overall Global Gender Gap Index")
+t2=which(GIto2$Subindicator.Type=="Index")
+t3=intersect(t1,t2)
+GIt2=GIto2[t3,]
+temp1=GIt2$X2019
+zwb=(temp1-mean(temp1,na.rm=TRUE))/sd(temp1,na.rm=TRUE)
+zwb_s=zwb*-1
+rm(temp1,t1,t2,t3,GIto2)
+
+GI=matrix(NA,length(COHT$Site))
+
+for(x in 1:length(GI)){
+  temp=COHT$Country[x]
+  if (temp=="USA"){
+    temp="United States"
+  } else if(temp=="UK"){
+    temp="United Kingdom"
+  }else if(temp=="Russia"){
+    temp="Russian Federation"
+  }else if(temp=="SKorea"){
+    temp="Korea (Republic of)"
+  }else if(temp=="Skorea"){
+    temp="Korea (Republic of)"
+  }
+  
+  temp2=which(GIt$COUNTRY==temp)
+  if(temp=="Korea (Republic of)"){
+    temp3=which(GIt2$Country.Name=="Korea, Rep.")
+  }else {
+    temp3=which(GIt2$Country.Name==temp)
+  }
+  
+  GI[x]=(zundp[temp2]+zwb_s[temp3])/2
+  rm(temp, temp2, temp3)
+}
+rm(zundp,zwb,zwb_s,GIt,GIt2)
+
+## GDP per capita
+# World Bank
+GDPo=read.csv(paste0(rootdir,'/','tables','/',"API_NY.GDP.PCAP.CD_DS2_en_csv_v2_3930492.csv"))
+GDP=matrix(NA,length(GI),1)
+for(x in 1:length(GI)){
+  temp=COHT$Country[x]
+  #print(temp)
+  if (temp=="USA"){
+    temp="United States"
+  } else if(temp=="UK"){
+    temp="United Kingdom"
+  }else if(temp=="Russia"){
+    temp="Russian Federation"
+  }else if(temp=="SKorea"){
+    temp="Korea, Rep."
+  }else if(temp=="Skorea"){
+    temp="Korea, Rep."
+  }
+  
+  temp2=which(GDPo$Country.Name==temp)
+  
+  GDP[x]=GDPo$X2019[temp2]
+  rm(temp,temp2)
+}
+rm(x,GDPo)
+
+# Relationship between GDP and GI in the sample
+GDPandGI=cor.test(GDP,GI)
+#
+#Now preparing Freesurfer and sex/age
+#
+#---------------------LOADING DATA---------------------------
+## DATA structure
+TEST<-read.csv(paste0(rootdir,'/','freesurfertables','/',COHT$Site[1],'/DATA.csv'))
+NTEMP=names(TEST)
+TH=4:73 # Thickness specific indices
+rm(TEST)
+
+## THICKNESS
+#Initialize variables
+# Variables Thickness (means and SD per gender)
+mTHm=matrix(NA,length(COHT$Site),length(TH))
+sdTHm=matrix(NA,length(COHT$Site),length(TH))
+mTHf=matrix(NA,length(COHT$Site),length(TH))
+sdTHf=matrix(NA,length(COHT$Site),length(TH))
+# Unresidualized (used for the analyses exploring changes in men and separately in women)
+mTHmur=matrix(NA,length(COHT$Site),length(TH)) # unresidualized
+sdTHmur=matrix(NA,length(COHT$Site),length(TH)) # idem
+mTHfur=matrix(NA,length(COHT$Site),length(TH)) # idem
+sdTHfur=matrix(NA,length(COHT$Site),length(TH)) # idem
+DEMt=matrix(NA,length(COHT$Site),9)
+DEMt[,1]=COHT$Site
+for(z in 1:length(COHT$Site)){
+  DT<-read.csv(paste0(rootdir,'/','freesurfertables','/',COHT$Site[z],'/DATA.csv'))
+  
+  ## Cleaning the data
+  # Loading matrix with QC measures
+  OUTL<-read.csv(paste0(rootdir,'/','freesurfertables','/',COHT$Site[z],'/DATA_outliers_cortical.csv'))
+  
+  # Excluding subjects after visual QC
+  te=which(DT$QCi>=3)
+  if(length(te)>0){
+    DT<-DT[-c(te),]
+    OUTL<-OUTL[-c(te),]
+  }
+  
+  # Excluding subjects after automatic QC
+  sp<-which(rowSums(OUTL)>0)
+  if(length(sp)>0){
+    DT<-DT[-sp,]
+  }
+  rm(OUTL,te,sp)
+  
+  im=which(DT$Sex=="m")
+  ife=which(DT$Sex=="f")
+  
+  #---------------Demographics of analysed data-----------
+  DEMt[z,2]=length(DT$S)
+  DEMt[z,3]=length(im)
+  DEMt[z,4]=length(ife)
+  # Ages disregard the months 
+  DEMt[z,5]=mean(floor(DT$Age))
+  DEMt[z,6]=max(floor(DT$Age))
+  DEMt[z,7]=min(floor(DT$Age))
+  DEMt[z,8]=mean(floor(DT$Age[ife]))
+  DEMt[z,9]=mean(floor(DT$Age[im]))
+  
+  # Regressing out the age
+  for (x in 1:length(TH)) {
+    a=DT[,TH[x]]
+    ch1=which(a==0) # just in case a region failed to be identified
+    if(length(ch1)>0){
+      print("DANGER")
+      TEST=rbind(TEST,c(z,x))
+    }
+    a[ch1]=NA
+    rm(ch1)
+    c=floor(DT$Age)
+    wisn=setdiff(1:length(a),which(is.na(a))) # otherwise it scrambles the vectors
+    lres=lm(formula= a ~ c )
+    
+    if( ( length(a)-length(wisn) ) >0 ){
+      lresiduals=matrix(NA,length(a),1)
+      lresiduals[wisn]=lres$residuals
+    }else{
+      lresiduals=lres$residuals
+    }
+    
+    mTHm[z,x]=mean(lresiduals[im],na.rm=TRUE)
+    sdTHm[z,x]=sd(lresiduals[im],na.rm=TRUE)
+    mTHf[z,x]=mean(lresiduals[ife],na.rm=TRUE)
+    sdTHf[z,x]=sd(lresiduals[ife],na.rm=TRUE)
+    
+    # Age not regressed out
+    mTHmur[z,x]=mean(a[im],na.rm=TRUE)
+    sdTHmur[z,x]=sd(a[im],na.rm=TRUE)
+    mTHfur[z,x]=mean(a[ife],na.rm=TRUE)
+    sdTHfur[z,x]=sd(a[ife],na.rm=TRUE)
+    
+    rm(a,c,lres,lresiduals,wisn)
+  }
+  rm(DT,ife,im,x)
+}
+rm(z)
+
+# Just cleaning up the Demographics and extended Demographics
+DEM=as.data.frame(DEMt)
+names(DEM)=c("Sample","N","NMale","NFem","AGEm","MaxAge","MinAge","FEM_AGEm","MALE_AGEm")
+rm(DEMt)
+
+# Excluding any subject with only males or females after clean-up
+toex=union(which(as.numeric(DEM$NFem)<2),which(as.numeric(DEM$NMale)<2))
+if(length(toex)>0){
+  mTHm<-mTHm[-c(toex),]
+  sdTHm<-sdTHm[-c(toex),]
+  mTHf<-mTHf[-c(toex),]
+  sdTHf<-sdTHf[-c(toex),]
+  DEM<-DEM[-c(toex),]
+  GI<-GI[-c(toex),]
+  GDP<-GDP[-c(toex),]
+  COHT<-COHT[-c(toex),]
+  mTHmur<-mTHmur[-c(toex),]
+  sdTHmur<-sdTHmur[-c(toex),]
+  mTHfur<-mTHfur[-c(toex),]
+  sdTHfur<-sdTHfur[-c(toex),]
+}
+numTH <- (length(TH))
+
+save(list = c("mTHm", "sdTHm", "mTHf", "sdTHf", "DEM", "GI", "GDP",
+              "COHT", "mTHmur", "sdTHmur", "mTHfur", "sdTHfur", "numTH"),
+     file = paste0(rootdir,"/","tables","/","dataformeta.Rdata"))
+
---
+++ code/Thickness-GII.R
@@ -0,0 +1,248 @@
+#!/usr/bin/env Rscript
+#Created by N. Crossley, U.C. Chile, modified by A. Zugman, NIMH/NIH
+#Meta-analysis and figure creation for 
+#"Country-level gender inequality is associated with structural differences in the 
+#brains of women and men"
+#
+#load packages
+library(metafor)
+library(ggplot2)
+library(egg)
+library(cowplot)
+library(ggseg)
+library(here)
+library(rstudioapi)
+
+#------------- INPUT NEEDED---------------------
+# Change the directory as needed
+if ( rstudioapi::isAvailable() ){
+  script_path <- dirname( rstudioapi::getActiveDocumentContext()$path )
+  rootdir <- dirname(script_path)
+  setwd (rootdir)
+} else {
+  script_path <- paste0(here(),"/","code","/")
+  rootdir <- dirname(script_path)
+  setwd (rootdir)
+}
+
+load(paste0(rootdir,"/","tables","/","dataformeta.Rdata"))
+
+
+#----------- Meta-analysis ---------------------
+Am=matrix(NA,numTH,2)
+for (z in 1:numTH){
+  M<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+  A<-rma.uni(yi=M$yi,vi=M$vi,mods=cbind(as.numeric(GI)),method = "PM")
+  Am[z,1]=A$beta[2]
+  Am[z,2]=A$pval[2]
+  rm(M,A)
+}
+rm(z)
+
+# For regional
+Am_ROI=Am
+Am_ROI=Am_ROI[-c(35,70),] # excluding the global measures for the FDR correction
+Am_fdr=p.adjust(Am_ROI[,2],"fdr")
+
+# Controlling for GDP
+Amg=matrix(NA,numTH,2)
+for (z in 1:numTH){
+  Mg<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+  Ag<-rma.uni(yi=Mg$yi,vi=Mg$vi,mods=cbind(as.numeric(GI),log(as.numeric(GDP))),method = "PM")
+  Amg[z,1]=Ag$beta[2]
+  Amg[z,2]=Ag$pval[2]
+  rm(Mg,Ag)
+}
+rm(z)
+Am_ROIg=Amg
+Am_ROIg=Am_ROIg[-c(35,70),]
+Am_fdrg=p.adjust(Am_ROIg[,2],"fdr")
+
+#----------------------------------- Figures -----------------------------------
+z=35
+M1<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+A1<-rma.uni(yi=M1$yi,vi=M1$vi,mods=as.numeric(GI),method = "PM")
+
+Tright1<-as.data.frame(cbind(as.numeric(GI),M1$yi,as.numeric(DEM$N)))
+Tright1$V3[which(as.numeric(DEM$N)>400)]<-400 # capping the N to 200 or 400? to improve visibility
+Pex_r1<-predict.rma(A1, newmods= rbind(cbind(-1.5,25),cbind(1,25)) )
+
+bp<- ggplot(Tright1, aes(x=V1, y=V2, size = V3)) +
+  geom_hline(colour="black", yintercept=0,size=0.6,linetype="dotted") +  
+  geom_point(alpha=0.3) + scale_size_area(max_size=15, name="Number of participants",breaks=c(100,200,300,400)) +
+  xlab("Gender Inequality (z-scores)") + ylab("Difference in cortical thickness (residuals)") + theme_light() + theme(legend.position="right") + 
+  annotate("text",x=1, y=-0.08, label= "paste(italic(P), \"-value = 0.006\")", parse = TRUE,size=7,hjust=1) + 
+  annotate("text",x=1, y=-0.095, label= "paste(italic(I) ^ 2, \" = 24.38%\")", parse = TRUE,size=7,hjust=1) + 
+  annotate("text",x=1, y=-0.11, label= "paste(italic(R) ^ 2, \" = 3.97%\")", parse = TRUE,size=7,hjust=1) + 
+  annotate("label",x=-1.3, y=-0.1, label= "FEMALE > MALE", parse = TRUE,size=7) + 
+  annotate("label",x=-1.3, y=0.1, label= "FEMALE < MALE", parse = TRUE,size=7) + 
+  ggtitle("A. Right Hemisphere Cortical Thickness") +
+  theme(plot.title=element_text(size=22, 
+                                face="bold", 
+                                hjust=0),  # title
+        axis.title.x=element_text(size=18),  # X axis title
+        axis.title.y=element_text(size=18),  # Y axis title
+        axis.text.x=element_text(size=15),  # X axis text
+        axis.text.y=element_text(size=15),
+        legend.title = element_text(size=19), 
+        legend.text = element_text(size=16),
+        legend.key=element_rect(fill='white'),
+        legend.position="bottom",
+        panel.background = element_rect(fill=NA,colour="grey")) +
+  geom_segment(aes(x =-1.5, y = Pex_r1$pred[1], xend = 1, yend = Pex_r1$pred[2]),size=1,linetype="solid",colour="black") 
+print(bp)
+
+#----------------------- ROI figures -------------------------------------------
+
+z=2
+M2<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+A2<-rma.uni(yi=M2$yi,vi=M2$vi,mods=as.numeric(GI),method = "PM")
+
+Tright2<-as.data.frame(cbind(as.numeric(GI),M2$yi,as.numeric(DEM$N)))
+Tright2$V3[which(as.numeric(DEM$N)>400)]<-400 # capping the N to 200 to improve visibility
+Pex_r2<-predict.rma(A2, newmods= rbind(cbind(-1.5,25),cbind(1,25)) )
+
+bp_r1<- ggplot(Tright2, aes(x=V1, y=V2, size = V3)) +
+  geom_hline(colour="black", yintercept=0,size=0.6,linetype="dotted") +  
+  geom_point(alpha=0.3) + scale_size_area(max_size=10, name="Number of participants",breaks=c(100,200,300,400)) +
+  xlab(" ") + ylab("Difference in cortical thickness") + theme_light() +  
+  annotate("text",x=0.3, y=-0.18, label= "paste(italic(P), \"-value (fdr) = 0.041\")", parse = TRUE,size=5) + 
+  annotate("text",x=0.2, y=-0.21, label= "paste(italic(I) ^ 2, \" = 16.89%, \")", parse = TRUE,size=5,hjust=1) + 
+  annotate("text",x=0.2, y=-0.21, label= "paste(italic(R) ^ 2, \" = 30.7%\")", parse = TRUE,size=5,hjust=0) + 
+  ylim(c(-0.21,0.22))+
+  ggtitle("A. RIGHT CAUDAL\n    ANTERIOR CINGULATE") +
+  theme(plot.title=element_text(size=15, 
+                                face="bold", 
+                                hjust=0),  # title
+        axis.title.x=element_text(size=13),  # X axis title
+        axis.title.y=element_text(size=13),  # Y axis title
+        axis.text.x=element_text(size=9),  # X axis text
+        axis.text.y=element_text(size=9),
+        legend.title = element_text(size=13), 
+        legend.text = element_text(size=12),
+        legend.key=element_rect(fill='white'),
+        legend.position="none",
+        panel.background = element_rect(fill=NA,colour="grey")) +
+  geom_segment(aes(x =-1.5, y = Pex_r2$pred[1], xend = 1, yend = Pex_r2$pred[2]),size=0.5,linetype="solid",colour="black") 
+rm(M2,A2)
+
+# right medial orbitofrontal
+z=13
+M2<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+A2<-rma.uni(yi=M2$yi,vi=M2$vi,mods=as.numeric(GI),method = "PM")
+
+Tright3<-as.data.frame(cbind(as.numeric(GI),M2$yi,as.numeric(DEM$N)))
+Tright3$V3[which(as.numeric(DEM$N)>400)]<-400 # capping the N to 200 to improve visibility
+Pex_r3<-predict.rma(A2, newmods= rbind(cbind(-1.5,25),cbind(1,25)) )
+
+bp_r2<- ggplot(Tright3, aes(x=V1, y=V2, size = V3)) +
+  geom_hline(colour="black", yintercept=0,size=0.6,linetype="dotted") +  
+  geom_point(alpha=0.3) + scale_size_area(max_size=10, name="Number of participants",breaks=c(100,200,300,400)) +
+  xlab("Gender Inequality (z-scores)") + ylab("Difference in cortical thickness") + theme_light() + 
+  annotate("text",x=0.3, y=-0.18, label= "paste(italic(P), \"-value (fdr) = 0.013\")", parse = TRUE,size=5) + 
+  annotate("text",x=0.3, y=-0.21, label= "paste(italic(I) ^ 2, \" = 20.92%, \")", parse = TRUE,size=5,hjust=1) + 
+  annotate("text",x=0.3, y=-0.21, label= "paste(italic(R) ^ 2, \" = 28.43%\")", parse = TRUE,size=5,hjust=0) + 
+  ylim(c(-0.21,0.22))+
+  ggtitle("B. RIGHT MEDIAL\n    ORBITOFRONTAL") +
+  theme(plot.title=element_text(size=15, 
+                                face="bold", 
+                                hjust=0),  # title
+        axis.title.x=element_text(size=13),  # X axis title
+        axis.title.y=element_text(size=13),  # Y axis title
+        axis.text.x=element_text(size=9),  # X axis text
+        axis.text.y=element_text(size=9),
+        legend.title = element_text(size=13), 
+        legend.text = element_text(size=12),
+        legend.key=element_rect(fill='white'),
+        legend.position="none",
+        panel.background = element_rect(fill=NA,colour="grey")) +
+  geom_segment(aes(x =-1.5, y = Pex_r3$pred[1], xend = 1, yend = Pex_r3$pred[2]),size=0.5,linetype="solid",colour="black") +
+  rm(M2,A2)
+
+# left lateral occipital
+z=45
+M2<-escalc("MD",m1i=mTHm[,z],m2i=mTHf[,z],sd1i=sdTHm[,z],sd2i=sdTHf[,z],n1i=as.numeric(DEM$NMale),n2i=as.numeric(DEM$NFem),vtype="LS")
+A2<-rma.uni(yi=M2$yi,vi=M2$vi,mods=as.numeric(GI),method = "PM")
+
+Tright4<-as.data.frame(cbind(as.numeric(GI),M2$yi,as.numeric(DEM$N)))
+Tright4$V3[which(as.numeric(DEM$N)>400)]<-400 # capping the N to 200 to improve visibility
+Pex_r4<-predict.rma(A2, newmods= rbind(cbind(-1.5,25),cbind(1,25)) )
+
+bp_r3<- ggplot(Tright4, aes(x=V1, y=V2, size = V3)) +
+  geom_hline(colour="black", yintercept=0,size=0.6,linetype="dotted") +  
+  geom_point(alpha=0.3) + scale_size_area(max_size=10, name="Number of participants",breaks=c(100,200,300,400)) +
+  xlab(" ") + ylab("Difference in cortical thickness") + theme_light() + 
+  annotate("text",x=0.3, y=-0.18, label= "paste(italic(P), \"-value (fdr) = 0.043\")", parse = TRUE,size=5) + 
+  annotate("text",x=0.3, y=-0.21, label= "paste(italic(I) ^ 2, \" = 18.07%, \")", parse = TRUE,size=5,hjust=1) + 
+  annotate("text",x=0.3, y=-0.21, label= "paste(italic(R) ^ 2, \" = 31.78%\")", parse = TRUE,size=5,hjust=0) + 
+  ylim(c(-0.21,0.22))+
+  ggtitle("C. LEFT LATERAL\n    OCCIPITAL") +
+  theme(plot.title=element_text(size=15, 
+                                face="bold", 
+                                hjust=0),  # title
+        axis.title.x=element_text(size=13),  # X axis title
+        axis.title.y=element_text(size=13),  # Y axis title
+        axis.text.x=element_text(size=9),  # X axis text
+        axis.text.y=element_text(size=9),
+        legend.title = element_text(size=13), 
+        legend.text = element_text(size=12),
+        legend.key=element_rect(fill='white'),
+        legend.position="none",
+        panel.background = element_rect(fill=NA,colour="grey")) +
+  geom_segment(aes(x =-1.5, y = Pex_r4$pred[1], xend = 1, yend = Pex_r4$pred[2]),size=0.5,linetype="solid",colour="black") 
+rm(M2,A2)
+
+someData = data.frame(region = c("caudal anterior cingulate","cuneus","fusiform","medial orbitofrontal","paracentral","posterior cingulate","isthmus cingulate","rostral anterior cingulate",
+                                 "superior frontal","precuneus","parahippocampal","pericalcarine","temporal pole",
+                                 "lingual","superior parietal","isthmus cingulate","superior frontal",
+                                 "corpus callosum","entorhinal"),p = c(1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2), stringsAsFactors = FALSE)
+AC=ggseg(.data=someData, atlas=dk, colour="black", size=0.3,mapping=aes(fill=p),hemisphere="right",view="medial")+theme_void()+theme(legend.position="none")+scale_fill_continuous(low="deepskyblue1", high="grey95")
+
+someData2 = data.frame(region = c("medial orbitofrontal","caudal anterior cingulate","cuneus","fusiform","paracentral","posterior cingulate","isthmus cingulate","rostral anterior cingulate",
+                                  "superior frontal","precuneus","parahippocampal","pericalcarine",
+                                  "lingual","superior parietal","lateral orbitofrontal","temporal pole","isthmus cingulate",
+                                  "corpus callosum","entorhinal"),p = c(1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2), stringsAsFactors = FALSE)
+#someData2 = data.frame(region = c("medial orbitofrontal"),  p = 10,  stringsAsFactors = FALSE)
+OF=ggseg(.data=someData2, atlas=dk, colour="black", size=0.3,mapping=aes(fill=p),hemisphere="right",view="medial")+theme_void()+theme(legend.position="none") +scale_fill_continuous(low="deepskyblue1", high="grey95")
+
+someData3 = data.frame(region = c("bankssts","caudal middle frontal", 
+                                  "lateral occipital","lateral orbitofrontal", "middle temporal",
+                                  "fusiform","inferior parietal","inferior temporal",
+                                  "pars opercularis","pars orbitalis" ,"pars triangularis",
+                                  "postcentral","precentral" ,"rostral middle frontal",
+                                  "superior frontal","superior parietal","superior temporal",
+                                  "supramarginal","temporal pole","transverse temporal","insula"
+),
+p=c(2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2),stringsAsFactors = FALSE)
+LO=ggseg(.data=someData3, atlas=dk, colour="black", size=0.3,mapping=aes(fill=p),hemisphere="left",view="lateral")+theme_void()+theme(legend.position="none") +scale_fill_continuous(low="deepskyblue1", high="grey95")
+
+
+BR=plot_grid(AC,OF,LO,nrow=1)
+
+title <- ggdraw() + 
+  draw_label(
+    "Regional differences in cortical thickness",
+    fontface = 'bold',x = 0,hjust = 0,size = 18) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+legend <- get_legend(
+  # create some space to the left of the legend
+  bp_r2 +
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+
+A=ggdraw(bp_r1)  +    draw_plot(AC,x=-0.18,y=.28,scale=0.38)
+B=ggdraw(bp_r2 + theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank(),axis.title.x=element_text(size=15))) + draw_plot(OF,x=-0.26,y=.28,scale=0.38) 
+C=ggdraw(bp_r3 +theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank() )) +draw_plot(LO,x=-0.26,y=.28,scale=0.38)
+
+F3c=plot_grid(A,B,C , align="vh",nrow=1)
+
+F3ct=plot_grid(title,legend,F3c,ncol=1,rel_heights=c(0.1,0.1,1))
+ggsave('fig2.pdf',plot=F3ct,path=paste0(rootdir,"/","figures","/"),dpi=800,width=14,height=7, units="in")
+command = paste0("open ",rootdir,"/","figures","/","fig2.pdf") 
+system(command)
+#print (F3ct)
